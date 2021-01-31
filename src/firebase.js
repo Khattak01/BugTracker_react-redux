a5f2c0f4b082c4994445fefe7b90839cbff7df66
Khattak01 <57499748+Khattak01@users.noreply.github.com>
2021-01-31 12:52:11 -0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,13 @@ import firebase from 'firebase'
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBNIAbgs_1ywZ52GNYPb5bEf3Hi851pYgc",
-    authDomain: "react-my-burger-9676f.firebaseapp.com",
-    databaseURL: "https://react-my-burger-9676f.firebaseio.com",
-    projectId: "react-my-burger-9676f",
-    storageBucket: "react-my-burger-9676f.appspot.com",
-    messagingSenderId: "551859769043",
-    appId: "1:551859769043:web:1e4ea6163d843bc255ce26",
-    measurementId: "G-LYT15SYF3W"
+    apiKey: "******",
+    authDomain: "******",
+    databaseURL: "******",
+    projectId: "*****",
+    storageBucket: "*****",
+    messagingSenderId: "*****",
+    appId: "*****
   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
